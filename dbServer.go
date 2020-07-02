462e468f07e947f028da2f4cc4200a80eff3cdc3
DCsunset <dcsunset@protonmail.com>
2020-07-02 07:04:53 -0700

    init: add basic grpc service for key value store

---
+++ dbServer.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/DCsunset/openwhisk-grpc/db"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	log.Println("Server listen at :9000")
+
+	server := db.Server{}
+	grpcServer := grpc.NewServer()
+	db.RegisterDbServiceServer(grpcServer, &server)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/DCsunset/openwhisk-grpc
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.4.1
+	google.golang.org/grpc v1.30.0
+	google.golang.org/protobuf v1.25.0
+)
