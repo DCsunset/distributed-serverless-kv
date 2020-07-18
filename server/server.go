ffed881e1b1b7a9dce0db7f821d5715fcc1b8723
DCsunset <dcsunset@protonmail.com>
2020-07-18 05:56:50 -0700

    feat(server): update gRPC interface

--- server/server.go
+++ server/server.go
@@ -4,24 +4,18 @@ import (
 	"context"
 
 	"github.com/DCsunset/openwhisk-grpc/db"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
-type Server struct {
-}
+type Server struct{}
 
-var store = make(map[string]string)
+var store = Store{}
 
 func (s *Server) Get(ctx context.Context, in *db.GetRequest) (*db.GetResponse, error) {
-	value, ok := store[in.Key]
-	if ok {
-		return &db.GetResponse{Value: value}, nil
-	}
-	return nil, status.Errorf(codes.NotFound, "Key not found")
+	value := store.Get(in.Key, in.Loc)
+	return &db.GetResponse{Value: value}, nil
 }
 
 func (s *Server) Set(ctx context.Context, in *db.SetRequest) (*db.SetResponse, error) {
-	store[in.Key] = in.Value
-	return &db.SetResponse{}, nil
+	loc := store.Set(in.Key, in.Value, in.VirtualLoc, in.Dep, in.VirtualDep)
+	return &db.SetResponse{Location: loc}, nil
 }
--- server/store.go
+++ server/store.go
@@ -1,7 +1,7 @@
 package main
 
 type Node struct {
-	dep  int
+	dep  int64
 	data map[string]string // record current updates
 }
 
@@ -9,16 +9,16 @@ type Store struct {
 	nodes []Node // all nodes
 }
 
-func (s *Store) newNode(dep int) int {
+func (s *Store) newNode(dep int64) int64 {
 	node := Node{
 		dep:  -1,
 		data: make(map[string]string),
 	}
 	s.nodes = append(s.nodes, node)
-	return len(s.nodes) - 1
+	return int64(len(s.nodes)) - 1
 }
 
-func (s *Store) Get(key string, loc int) string {
+func (s *Store) Get(key string, loc int64) string {
 	node := s.nodes[loc]
 	// Find till root
 	for _, ok := node.data[key]; !ok && node.dep != -1; {
@@ -27,7 +27,7 @@ func (s *Store) Get(key string, loc int) string {
 	return node.data[key]
 }
 
-func (s *Store) Set(key string, value string, virtualLoc int, dep int, virtualDep int) int {
+func (s *Store) Set(key string, value string, virtualLoc int64, dep int64, virtualDep int64) int64 {
 	newLoc := s.newNode(dep)
 	node := s.nodes[newLoc]
 	node.data[key] = value
