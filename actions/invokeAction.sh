a261d48f4c3ebf4e2c316ef59cd71f538e63fd38
DCsunset <dcsunset@protonmail.com>
2020-07-21 19:30:26 -0700

    feat: update action scripts

--- actions/invokeAction.sh
+++ actions/invokeAction.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export APIHOST=172.18.0.4:31001
 export AUTH="23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP"
-export ACTION=test-grpc
+export ACTION=mapreduce
 
-BODY='{ "low": 0, "high": 20, "parallel": true }'
+BODY='{ "kind": "runner" }'
 
 curl --insecure -X POST -u $AUTH -H "Content-Type: application/json" -d "$BODY" "https://$APIHOST/api/v1/namespaces/guest/actions/$ACTION?blocking=true&result=true"
