a15aeeb84e239f7e63e2131404df8a7254c021a4
DCsunset <dcsunset@protonmail.com>
2020-07-29 01:18:49 -0700

    feat(storage): add the isomorphic Merkle tree and fix a bug

--- storage/store.go
+++ storage/store.go
@@ -1,8 +1,15 @@
 package storage
 
+import (
+	"crypto/sha256"
+	"encoding/json"
+)
+
 type Node struct {
-	dep  int64
-	data map[string]string // record current updates
+	dep      int64
+	children []int64
+	data     map[string]string // record current updates
+	digest   []byte            // Save Merkle tree inline
 }
 
 type Store struct {
@@ -13,11 +20,18 @@ type Store struct {
 
 func (s *Store) newNode(dep int64, data map[string]string) int64 {
 	node := Node{
-		dep:  -1,
-		data: data,
+		dep:      dep,
+		data:     data,
+		children: nil,
 	}
 	s.Nodes = append(s.Nodes, node)
-	return int64(len(s.Nodes)) - 1
+	newLoc := int64(len(s.Nodes)) - 1
+
+	// Append to parent's child list
+	parent := s.Nodes[newLoc]
+	parent.children = append(parent.children, newLoc)
+
+	return newLoc
 }
 
 func (s *Store) Get(id int64, keys []string, loc int64, virtualLoc int64) map[string]string {
@@ -65,6 +79,11 @@ func (s *Store) addToLocMap(id int64, virtualLoc int64, loc int64) {
 }
 
 func (s *Store) Set(id int64, data map[string]string, virtualLoc int64, dep int64, virtualDep int64) int64 {
+	// Compute hash
+	hash := sha256.New()
+	bytes, _ := json.Marshal(data)
+	digest := hash.Sum(bytes)
+
 	var newLoc int64
 	if dep != -2 {
 		newLoc = s.newNode(dep, data)
@@ -73,5 +92,25 @@ func (s *Store) Set(id int64, data map[string]string, virtualLoc int64, dep int6
 		realDep := s.LocMap[id][virtualDep]
 		newLoc = s.newNode(realDep, data)
 	}
+
+	s.updateHash(newLoc, digest)
+
 	return newLoc
+}
+
+func (s *Store) updateHash(loc int64, currentDigest []byte) {
+	node := s.Nodes[loc]
+	// Update till root
+	for {
+		hash := sha256.New()
+		for _, child := range node.children {
+			// Concatenate all digests
+			hash.Write(s.Nodes[child].digest)
+		}
+		node.digest = hash.Sum(currentDigest)
+		if node.dep == -1 {
+			break
+		}
+		node = s.Nodes[node.dep]
+	}
 }
