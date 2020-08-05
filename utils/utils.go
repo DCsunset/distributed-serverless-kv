84b25c68dffe4d35b0ab963c83d125ba514e836c
DCsunset <dcsunset@protonmail.com>
2020-08-05 22:42:54 +0800

    feat(utils): add PrintMap

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,19 @@
 package utils
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 func MakeRange(min, max int64) []int64 {
 	a := make([]int64, max-min)
 	for i := range a {
 		a[i] = min + int64(i)
 	}
 	return a
+}
+
+func PrintMap(m map[string]string) {
+	str, _ := json.Marshal(m)
+	fmt.Println(string(str))
 }
