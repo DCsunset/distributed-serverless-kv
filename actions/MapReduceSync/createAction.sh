f4c3b110fb9f5ba56c20f73ed1f44485581d0fa9
DCsunset <dcsunset@protonmail.com>
2020-08-08 10:12:51 +0800

    fix(actions): update scripts

--- actions/MapReduceSync/createAction.sh
+++ actions/MapReduceSync/createAction.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 export APIHOST=172.18.0.4:31001
 export AUTH="23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP"
-export ACTION=mapreduce
+export ACTION=mapreducesync
+
+CGO_ENABLED=0 go build -o exec main.go
+zip exec.zip exec
 
 echo "{\"namespace\": \"guest\", \"actionName\": \"$ACTION\", \"exec\": { \"kind\": \"blackbox\", \"image\": \"openwhisk/dockerskeleton\", \"binary\": true, \"code\": \"$(base64 exec.zip)\"} }" | \
 curl --insecure -X PUT -u $AUTH -H "Content-Type: application/json" -d @- https://$APIHOST/api/v1/namespaces/guest/actions/$ACTION?overwrite=true
+
+rm exec.zip exec
---
+++ actions/MapReduceSync/invokeAction.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export APIHOST=172.18.0.4:31001
+export AUTH="23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP"
+export ACTION=mapreducesync
+
+BODY='{ "kind": "runner", "location": 5432899587278703275 }'
+
+curl --insecure -X POST -u $AUTH -H "Content-Type: application/json" -d "$BODY" "https://$APIHOST/api/v1/namespaces/guest/actions/$ACTION?blocking=true&result=true"
