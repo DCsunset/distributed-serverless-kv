ba8c48f2d3e0f31e8877d87c6566361421cfb03e
DCsunset <dcsunset@protonmail.com>
2020-09-06 17:11:55 +0800

    chore: remove MapReduce actions

--- actions/MapReduce/invokeAction.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-export APIHOST=172.18.0.4:31001
-export AUTH="23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP"
-export ACTION=mapreduce
-
-BODY='{ "kind": "runner", "location": -7441470632338879788 }'
-
-time curl --insecure -X POST -u $AUTH -H "Content-Type: application/json" -d "$BODY" "https://$APIHOST/api/v1/namespaces/guest/actions/$ACTION?blocking=true&result=true"
