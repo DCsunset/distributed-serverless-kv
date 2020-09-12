2ebde59a855534035f6f45ac82fa8c7c0c6862fb
DCsunset <dcsunset@protonmail.com>
2020-09-12 22:27:31 +0800

    fix(benchmark): fix createAction.sh

--- benchmark/createAction.sh
+++ benchmark/createAction.sh
@@ -4,7 +4,7 @@ export APIHOST=aqua02:31001
 export AUTH="23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP"
 export ACTION=benchmark
 
-CGO_ENABLED=0 go build -o exec main.go
+CGO_ENABLED=0 go build -o exec benchmark.go
 zip exec.zip exec
 
 echo "{\"namespace\": \"guest\", \"actionName\": \"$ACTION\", \"exec\": { \"kind\": \"blackbox\", \"image\": \"openwhisk/dockerskeleton\", \"binary\": true, \"code\": \"$(base64 exec.zip)\"} }" | \
